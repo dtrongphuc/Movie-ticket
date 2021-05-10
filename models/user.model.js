b649363aadce51d8d2e4e0346f238fbb6841bb52
dtrongphuc <dtrongphucdv@gmail.com>
2021-05-10 21:30:58 +0700

    db connect

---
+++ models/user.model.js
@@ -0,0 +1,61 @@
+const user = (sequelize, DataTypes) => {
+	const User = sequelize.define('user', {
+		id: {
+			type: DataTypes.UUID,
+			allownull: false,
+			primaryKey: true,
+			defaultValue: DataTypes.UUIDV4,
+		},
+		email: {
+			type: DataTypes.STRING,
+			allownull: false,
+			unique: true,
+			validate: {
+				notEmpty: true,
+				isEmail: true,
+			},
+		},
+		hashedPassword: {
+			type: DataTypes.STRING,
+			allownull: false,
+		},
+		fullname: {
+			type: DataTypes.STRING,
+			allownull: false,
+		},
+		fullname: {
+			type: DataTypes.STRING,
+			allownull: false,
+		},
+		phoneNumber: {
+			type: DataTypes.STRING,
+			allownull: false,
+		},
+		role: {
+			type: DataTypes.STRING,
+			allownull: false,
+			validate: {
+				isIn: [['admin', 'member']],
+			},
+		},
+	});
+
+	// Khoá ngoại
+	// User.associate = models => {
+	//   User.hasMany(models.Booking)
+	// }
+
+	User.findByEmail = async (email) => {
+		let result = await User.findOne({
+			where: {
+				email: email,
+			},
+		});
+
+		return result;
+	};
+
+	return User;
+};
+
+module.exports = user;
