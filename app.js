870dd83a433abeae5ce85e3998f9f13967973ea3
dtrongphuc <dtrongphucdv@gmail.com>
2021-05-12 10:23:07 +0700

    passport

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
-const connection = require('./db/connection');
+const models = require('./db/connection');
 var expressLayouts = require('express-ejs-layouts');
+const passport = require('passport');
+const session = require('express-session');
 const app = express();
 const route = require('./routers/index');
 const cinemaRouter = require('./routers/admin/cinema');
@@ -13,13 +15,25 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.set('trust proxy', 1); // trust first proxy
+app.use(
+	session({
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized: false,
+		// cookie: { secure: true },
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', (req, res) => res.send('index'));
 
 // Test giao diện content
 app.get('/view', (req, res) => res.render('content/content'));
 
 route(app);
-
 
 //apply template
 app.use(expressLayouts);
