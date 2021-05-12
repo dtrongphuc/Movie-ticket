174d48b771e63e10dee27232332b7a3b3c5340fd
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-05-12 20:39:50 +0700

    push pull

--- controllers/User.Controller.js
+++ controllers/User.Controller.js
@@ -1,17 +1,28 @@
-const { User } = require("../db/connection");
-const bcrypt = require('bcrypt');
+const { User, Cinema, Booking, Ticket, Showtime, Movie,Theater } = require("../db/connection");
+const bcrypt = require("bcrypt");
 
 const comparePassword = async (plainPassword, hashedPassword) => {
   let check = await bcrypt.compare(plainPassword, hashedPassword);
   return check;
 };
 const hashPassword = async (plainPassword) => {
-	let hashedPassword = await bcrypt.hash(plainPassword, 10);
-	return hashedPassword;
+  let hashedPassword = await bcrypt.hash(plainPassword, 10);
+  return hashedPassword;
 };
 class UserController {
   async profile(req, res, next) {
     const { id } = req.params;
+
+    //test
+    const joinBooking = await Booking.findAll({
+      // include: [
+      //   {
+      //     model: User,
+      //     required: true
+      //   },
+      // ],
+    });
+
 
     User.findOne({ where: { id: id } })
       .then((user) => {
@@ -37,28 +48,30 @@ class UserController {
   }
   async changePass(req, res, next) {
     const formData = req.body;
-    let err_oldPass = '';
-    let err_newPass = '';
-    let err_confirm = '';
+    let err_oldPass = "";
+    let err_newPass = "";
+    let err_confirm = "";
     User.findOne({ where: { id: "32465435-f596-4f39-985e-eae06a589b2b" } })
       .then(async (user) => {
-        if (!(await comparePassword(formData.old_password, user.hashedPassword))) {
-			err_oldPass = 'Mật khẩu củ không chính xác';
-		}
-		if(formData.new_password != formData.confirm){
-			err_confirm = 'Mật khẩu mới không khớp';
-		}
+        if (
+          !(await comparePassword(formData.old_password, user.hashedPassword))
+        ) {
+          err_oldPass = "Mật khẩu củ không chính xác";
+        }
+        if (formData.new_password != formData.confirm) {
+          err_confirm = "Mật khẩu mới không khớp";
+        }
 
-		if(!err_oldPass && !err_newPass && !err_confirm ){
-			let new_hassPassword = await hashPassword(formData.new_password);
-			user.hashedPassword = new_hassPassword;
-			user.save();
-			return res.status(200).json();
-		}
-		else {
-			return res.status(400).json({err_newPass, err_oldPass, err_confirm});
-		}
-        
+        if (!err_oldPass && !err_newPass && !err_confirm) {
+          let new_hassPassword = await hashPassword(formData.new_password);
+          user.hashedPassword = new_hassPassword;
+          user.save();
+          return res.status(200).json();
+        } else {
+          return res
+            .status(400)
+            .json({ err_newPass, err_oldPass, err_confirm });
+        }
       })
       .catch(() => res.status(400).json());
 
