e4c3f428df8d0d97fcc9c5f583d2ecd8b1861f61
ThanhPhu-Dev <tphu2000@gmail.com>
2021-05-14 08:48:52 +0700

    merge

--- app.js
+++ app.js
@@ -5,16 +5,6 @@ const express = require('express'),
 	app = express(),
 	route = require('./routers/index'),
 	{ initPassport } = require('./middlewares/passport/index'),
-<<<<<<< HEAD
-
-=======
-	theatersRouter = require('./routers/admin/theaters'),
-	cinemaRouter = require('./routers/admin/cinema'),
-	showtimeRouter = require('./routers/admin/showtime'),
-	statisticeRouter = require('./routers/admin/statistics'),
-	movieRouter = require('./routers/admin/movie'),
-	homeRouter = require('./routers/home');
->>>>>>> 8db795158596663d584cca93b0ab27a5adb769d0
 	port = 3000;
 
 // APP CONFIGURE
@@ -31,28 +21,8 @@ initPassport(app);
 
 
 route(app);
-
-<<<<<<< HEAD
-=======
-//----------------------------------------------------------- ADMIN --------------------------------------------------------------
-
-app.get('/admin/dang-nhap', function (req, res) {
-	res.render('admin/login');
-});
-
-//apply template
-app.use(expressLayouts);
-app.set('layout', 'admin/layout');
-app.use('/admin/', cinemaRouter);
-app.use('/admin/suat-phim', showtimeRouter);
-app.use('/admin/cum-rap', theatersRouter);
-app.use('/admin/thong-ke', statisticeRouter);
-app.use('/admin/phim', movieRouter);
-
 // home
-app.use('/home', homeRouter);
-
->>>>>>> 8db795158596663d584cca93b0ab27a5adb769d0
+//app.use('/home', homeRouter);
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
