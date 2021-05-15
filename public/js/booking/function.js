fcdbcd346638cbf404c3eb5ad134b13d32de97e4
dtrongphuc <dtrongphucdv@gmail.com>
2021-05-15 11:46:27 +0700

    slider date not binding database

---
+++ public/js/booking/function.js
@@ -0,0 +1,64 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+class Booking {
+	// Date picker
+	datePicker = () => {
+		$$('.time__box:not(.no__showtime)')?.forEach((element) => {
+			element.addEventListener('click', (e) => {
+				// remove current active
+				$('.date--active').classList.remove('date--active');
+				// add active to selected date
+				element.querySelector('.time__box--date').classList.add('date--active');
+			});
+		});
+	};
+
+	// Theater picker
+	theaterPicker = () => {
+		$$('.theater__item')?.forEach((element) => {
+			element.addEventListener('click', (e) => {
+				// remove current active
+				$('.theater__item--active').classList.remove('theater__item--active');
+				// add active to selected theater
+				element.classList.add('theater__item--active');
+			});
+		});
+	};
+
+	// Movie picker
+	moviePicker = () => {
+		$$('.movie__item')?.forEach((element) => {
+			element.addEventListener('click', (e) => {
+				// remove current active
+				$('.movie__item--active').classList.remove('movie__item--active');
+				// add active to selected theater
+				element.classList.add('movie__item--active');
+			});
+		});
+	};
+
+	// Cinema picker
+	cinemaPicker = () => {
+		$$('.cinema__box:not(.cinema__box--disable)')?.forEach((element) => {
+			element.addEventListener('click', (e) => {
+				// remove current active
+				$('.cinema__box--active').classList.remove('cinema__box--active');
+				// add active to selected theater
+				element.classList.add('cinema__box--active');
+			});
+		});
+	};
+
+	innerSelectedDate = () => {
+		let day = $('.time__box--date.date--active')?.textContent;
+		let month = $('.time__box > .date--active')?.querySelector(
+			'.time__box--dayofweek'
+		)?.textContent;
+		// let year = $('.calendar__MM > .month');
+
+		console.log(`${day}/${month}`);
+	};
+}
+
+export default new Booking();
