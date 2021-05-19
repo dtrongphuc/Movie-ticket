0e2a86d74727078921bb7c37223826d785b075c2
ThanhPhu-Dev <tphu2000@gmail.com>
2021-05-19 13:20:21 +0700

    giua currenUser

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const express = require('express'),
 	{ initPassport } = require('./middlewares/passport/index'),
 	homeRouter = require('./routers/home');
 port = 3000;
-var bodyParser = require('body-parser');
-
 
 // APP CONFIGURE
 // app.use(require('morgan')('tiny'));
@@ -16,18 +14,23 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.locals.moment = require('moment');
 app.use(cookieParser());
-app.use(bodyParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('trust proxy', 1); // trust first proxy
 
 // Passport
 initPassport(app);
-
 // home
-app.use('/', homeRouter);
+// Custom flash middleware -- from Ethan Brown's book, 'Web Development with Node & Express'
+app.use(function (req, res, next) {
+	// if there's a flash message in the session request, make it available in the response, then delete it
+	res.locals.currentUser = null;
+	res.locals.message = req.session.message;
+	delete req.session.message;
+	next();
+});
 
-
+app.use('/home', homeRouter);
 //---------------------------------------------------------------- ADMIN -----------------------------------------------------
 route(app);
 
