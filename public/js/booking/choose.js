2d33a22d1f727399c70548e5197a378d962b5716
vthanhdo2000 <vthanhdo2000@gmail.com>
2021-05-20 23:43:51 +0700

    bookseat

--- public/js/booking/choose.js
+++ public/js/booking/choose.js
@@ -1,8 +1,59 @@
 
-const btn = document.getElementsByClassName("seatnumber");
+function onLoaderFunc()
+{
+  $(".seatStructure *").prop("disabled", true);
+}
 
 
-btn.addEventListener("click", (e) => {
-  btn.toggleClass('select');
+function takeData()
+{
+  if (( $("#Numseats").val() == 0 ))
+  {
+    alert("Vui lòng chọn số lượng ghế");
+  }
+  else
+  {
+    $(".seatStructure *").prop("disabled", false);
+    document.getElementById("notification").innerHTML = "<b style='margin-bottom:0px;background:yellow;'>Vui lòng chọn ghế!</b>";
+  }
+}
 
+function updateTextArea() { 
+    
+  if ($("input:checked").length == ($("#Numseats").val()))
+    {
+      $(".seatStructure *").prop("disabled", true);
+      
+     var allSeatsVals = [];
+  
+     $('#seatsBlock :checked').each(function() {
+       allSeatsVals.push($(this).val());
+     });
+
+     $('#seatsDisplay').val(allSeatsVals);
+
+    }
+  else
+    {
+      alert("Please select " + ($("#Numseats").val()) + " seats")
+    }
+  }
+
+
+function myFunction() {
+  alert($("input:checked").length);
+}
+
+
+
+$(":checkbox").click(function() {
+  if ($("input:checked").length == ($("#Numseats").val())) {
+    $(":checkbox").prop('disabled', true);
+    $(':checked').prop('disabled', false);
+  }
+  else
+    {
+      $(":checkbox").prop('disabled', false);
+    }
 });
+
