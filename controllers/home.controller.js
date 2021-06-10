2cd0c50f6caf1d8a83f465f73a29fba022b9ee5d
dtrongphuc <dtrongphucdv@gmail.com>
2021-06-10 20:17:33 +0700

    fix home page

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -5,66 +5,59 @@ const { movie } = require('./statiscics.admin.controller');
 const image = require('../models/image.model');
 
 class HomeController {
-
-    async index(req, res, next) {
-        
-        models.Movie.findAll({
-           include: [{
-               model: models.Image,
-               where: {},
-
-           }],
-		})
-		.then(movies => {
-			return res.render('content/content', {movies:movies});
-		})
-		.catch(() => {
-			res.status(500).send({ error: 'Something failed!' })
-		})
-    };
-
-	async indexNew(req, res, next) {
-        
-        models.Movie.findAll({
-           include: [{
-
-               model: models.Image,
-               where: {},
-
-           }],
-		   order: [
-			['openingDay', 'DESC'],
-			],
-		})
-		.then(movies => {
-			return res.render('content/contentNew', {movies:movies});
-		})
-		.catch(() => {
-			res.status(500).send({ error: 'Something failed!' })
-		})
-    };
-
-    async detail(req, res, next) {
-
-		const id = req.params.id;
-		
-		models.Movie.findOne({
-			where: {id: id},
+	async index(req, res, next) {
+		models.Movie.findAll({
 			include: [
 				{
 					model: models.Image,
-				}
-			]
+					where: {},
+				},
+			],
 		})
-		.then(movie => {
-			return res.render('content/detail', {movie : movie});
-		})
-		.catch(()=>{
-			res.status(500).send({ error: 'Something failed!' })
-		})
-			
+			.then((movies) => {
+				return res.render('content/content', { movies: movies });
+			})
+			.catch(() => {
+				res.status(500).send({ error: 'Something failed!' });
+			});
 	}
 
-};
+	async indexNew(req, res, next) {
+		models.Movie.findAll({
+			include: [
+				{
+					model: models.Image,
+					where: {},
+				},
+			],
+			order: [['openingDay', 'DESC']],
+		})
+			.then((movies) => {
+				return res.render('content/contentNew', { movies: movies });
+			})
+			.catch(() => {
+				res.status(500).send({ error: 'Something failed!' });
+			});
+	}
 
-module.exports = new HomeController
+	async detail(req, res, next) {
+		const id = req.params.id;
+
+		models.Movie.findOne({
+			where: { id: id },
+			include: [
+				{
+					model: models.Image,
+				},
+			],
+		})
+			.then((movie) => {
+				return res.render('content/detail', { movie: movie });
+			})
+			.catch(() => {
+				res.status(500).send({ error: 'Something failed!' });
+			});
+	}
+}
+
+module.exports = new HomeController();
