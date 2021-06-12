311a2b57fc2cdde3d3e1feeb6493091f887f7f09
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-06-12 09:41:57 +0700

    sửa lại detail

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -49,7 +49,7 @@ class HomeController {
 			],
 		})
 			.then((movie) => {
-				return res.render('content/detail', { movie: movie });
+				return res.render('movie/detail', { movie: movie });
 			})
 			.catch(() => {
 				res.status(500).send({ error: 'Something failed!' });
