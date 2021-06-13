6b1d6d701d09ae799e6fe145da4d42804cc277cf
Dương Trọng Phúc <dtrongphucdv@gmail.com>
2021-06-13 12:08:20 +0700

    handle load more hot movie

--- controllers/api/home.controller.js
+++ controllers/api/home.controller.js
@@ -12,7 +12,6 @@ module.exports = {
 		try {
 			// skip $offset, fetch $limit item
 			const { offset, limit } = req.query;
-
 			const movies = await Movie.findAll({
 				where: {
 					openingDay: {
@@ -68,6 +67,59 @@ module.exports = {
 			// });
 
 			res.status(200).json({ movies });
+		} catch (error) {
+			console.log(error);
+			res.status(400).json({
+				error,
+			});
+		}
+	},
+
+	countHotMovie: async (req, res) => {
+		try {
+			const movies = await Movie.findAll({
+				where: {
+					openingDay: {
+						[Op.lt]: Date.now(),
+					},
+				},
+				attributes: {
+					include: [
+						[
+							Sequelize.fn(
+								'COUNT',
+								Sequelize.col('showtimes->booking->ticket.id')
+							),
+							'sold',
+						],
+					],
+				},
+				include: [
+					{
+						model: Showtime,
+						attributes: [],
+						include: [
+							{
+								model: Booking,
+								attributes: [],
+								include: [
+									{
+										model: Ticket,
+										attributes: [],
+									},
+								],
+							},
+						],
+					},
+				],
+				group: ['movie.id'],
+				order: [[sequelize.literal('sold'), 'DESC']],
+				subQuery: false,
+			});
+
+			return res.status(200).json({
+				count: movies.length,
+			});
 		} catch (error) {
 			console.log(error);
 			res.status(400).json({
--- routers/api/home.js
+++ routers/api/home.js
@@ -3,5 +3,6 @@ const homeController = require('../../controllers/api/home.controller');
 const router = express.Router();
 
 router.get('/movie/hot', homeController.topMovie);
+router.get('/movie/count/hot', homeController.countHotMovie);
 
 module.exports = router;
